ba041c9cf72cba4fede09b3c0d3276b4a827b4ea
rs3254 <raysabb3254@gmail.com>
2018-06-09 19:17:50 -0400

    Database added

--- main.py
+++ main.py
@@ -7,9 +7,15 @@ from pymongo import MongoClient
 
 
 
+def read():
+	database = db.database1.find()
+	for item in database:
+		print(item)
+
+
 client = MongoClient()
 client = MongoClient('localhost', 27017)
-db = client.pymongo_test
+db = client.database1
 
 
 
@@ -24,14 +30,7 @@ headers3  = html.find_all("a", class_="single-story-module__headline-link")
 headerArr = []; 
 
 
-# posts = db.posts
-# post_data = {
-#     'title': 'Python and MongoDB',
-#     'content': 'PyMongo is fun, you guys',
-#     'author': 'Scott'
-# }
-# result = posts.insert_one(post_data)
-# print('One post: {0}'.format(result.inserted_id))
+
 
 
 
@@ -55,13 +54,24 @@ for j in headers3:
 for j in range(0, len(headerArr)):
 	timeStamp = datetime.datetime.utcnow()
 	headerString = re.sub(' +',' ',headerArr[j])
-	dictionary[timeStamp] = headerString
+	stringT = str(timeStamp).replace(".", "")
+	dictionary[stringT] = headerString
+	db.database1.insert_one(dictionary)
+	dictionary.clear()
+
+
+
+read()
+
+
 
 
 
 
 # print(dictionary.values())
 
-for j in dictionary:
-	print(dictionary[j])
+
+
+# for j in dictionary:
+# 	print(dictionary[j])
 
